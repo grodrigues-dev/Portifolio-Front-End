b9c2b3af54257ecda87e280860ea999b2527a312
grodrigues-dev <52932384+grodrigues-dev@users.noreply.github.com>
2020-01-06 10:00:04 -0300

    desativando botões no limite do carrossel

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 function slide02() {
     let background = document.querySelector('.container');
+    let btnNext = document.querySelector('#btnNext');
+    let btnPrevious = document.querySelector('#btnPrevious');
     if (background.className=='container'){
         background.classList.add("slide02")
+        btnNext.classList.add("active"); 
     } else {
         background.classList.replace("slide01", "slide02")
-    }
+        btnNext.classList.add("active"); 
+        btnPrevious.classList.remove("active")
+        
+    }   
     
 }
 
 function slide01() {
     let background = document.querySelector('.container');
-    background.classList.replace("slide02", "slide01")
-    console.log('teste1');
-
+    let btnNext = document.querySelector('#btnNext');
+    let btnPrevious = document.querySelector('#btnPrevious');
+    background.classList.replace("slide02", "slide01");
+    btnPrevious.classList.add("active"); 
+    btnNext.classList.remove("active");
 }
 
